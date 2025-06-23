223f57861ab2b8a45b2a3fb6a26d5c3a21f4e0e5
Omar Salloum <omarsalloum08@gmail.com>
2025-06-23 17:06:08 +0200

    adding leaflet map

--- Bookify_App/src/components/VenueTabs/VenueMapTab.tsx
+++ Bookify_App/src/components/VenueTabs/VenueMapTab.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import styles from "./VenueMapTab.module.css";
 
 interface Coordinates {
     lat: number;
@@ -9,11 +12,33 @@ interface VenueMapTabProps {
     coordinates: Coordinates;
 }
 
-const VenueMapTab: React.FC<VenueMapTabProps> = ({coordinates}) => {
+// Fix default marker icon bug in Leaflet + Webpack
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const VenueMapTab = ({coordinates}: VenueMapTabProps) => {
+    const {lat, lng} = coordinates;
+
     return (
-        <div>
-            <p>Map for lat: {coordinates.lat}, lng: {coordinates.lng}</p>
-            {/* Replace with actual Google Maps integration */}
+        <div className={styles.mapWrapper}>
+            <MapContainer
+                center={[lat, lng]}
+                zoom={13}
+                scrollWheelZoom={false}
+                className={styles.map}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[lat, lng]}>
+                    <Popup>This venue is here!</Popup>
+                </Marker>
+            </MapContainer>
         </div>
     );
 };
