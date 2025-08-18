32c9cfed8f5f7c410701fe995e82a98a55a61704
Omar Salloum <omarsalloum08@gmail.com>
2025-08-18 13:20:57 +0200

    adding prettier and modifying eslint

--- Bookify_App/src/App.tsx
+++ Bookify_App/src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
@@ -7,20 +7,20 @@ import VenueDetailsPage from "./pages/VenueDetailsPage/VenueDetailsPage";
 
 function App() {
 
-    return (
-            <Router>
-                <div className="app-container">
-                    <Navbar/>
-                    <main>
-                        <Routes>
-                            <Route path="/" element={<HomePage/>}/>
-                            <Route path="/venue/:id" element={<VenueDetailsPage/>}/>
-                        </Routes>
-                    </main>
-                    <Footer/>
-                </div>
-            </Router>
-    )
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/venue/:id" element={<VenueDetailsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
